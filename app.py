814e586d6f9526323e2bc7955fd438eae15ce2fc
joshiaman112 <joshiaman112@gmail.com>
2025-04-04 14:27:40 -0400

    Initial Setup

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from borda ECS Container."
+    return "Hello from joshi ECS Container."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
