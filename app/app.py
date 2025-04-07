3ded6a396c015dcb223925e7324f8a9ff134ede5
joshiaman112 <joshiaman112@gmail.com>
2025-04-07 17:16:25 -0400

    Code change for cloud run

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello from Aman Joshi ECS Container!!!"
+    return "Hello from Flask!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
