b57241ce34b9747fefbc953e6fe103fbcdbf1ea4
joshiaman112 <joshiaman112@gmail.com>
2025-04-08 01:51:12 -0400

    Updated app file

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask!"
+    return "Hello from the Joshi ECS container"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
